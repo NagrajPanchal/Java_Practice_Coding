2cab39c3479d5f37cf94c8003934e08550e02b21
Nagraj <nagrajsubhashpanchal.gmail.com>
2022-09-15 13:35:28 +0530

    Refactor : Print Numbers in decrement 100 to 1 and count how much time it will print

---
+++ src/com/bridgelabz/ForLoop100to1.java
@@ -0,0 +1,16 @@
+// Print number 100 to 1
+package com.bridgelabz;
+
+public class ForLoop100to1
+{
+    public static void main(String[] args)
+    {
+        int count = 0;
+    for (int i = 100; i >= 1; i--)
+    {
+        System.out.print(i + " ");
+        count++;
+    }
+       System.out.println("\nCount is : "+count);
+    }
+}
