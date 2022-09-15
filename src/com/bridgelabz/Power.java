e9e1795e2d76e804e36e57a056913cdcae740156
Nagraj <nagrajsubhashpanchal.gmail.com>
2022-09-15 15:34:01 +0530

    Refactor : Power of Number

---
+++ src/com/bridgelabz/Power.java
@@ -0,0 +1,23 @@
+package com.bridgelabz;
+
+public class Power
+{
+    public static void main(String[] args)
+    {
+
+    int num =5;
+    int p = 2;
+    int power = 1;
+    for(int i=1; i<=p; i++)
+    {
+        power = power * num;
+    }
+        System.out.println("Power is : " +power);
+    /*
+        int num = 6;
+        System.out.println(num*num);
+    */
+    }
+
+
+}
