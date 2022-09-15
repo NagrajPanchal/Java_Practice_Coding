2e578f1b7615f8d91fc3ccb03d73b581a9906a6d
Nagraj <nagrajsubhashpanchal.gmail.com>
2022-09-15 14:43:08 +0530

    Refactor : Multiplication

--- src/com/bridgelabz/Multiplication.java
+++ src/com/bridgelabz/Multiplication.java
@@ -1,15 +1,18 @@
 // Multiplication Table
 package com.bridgelabz;
-
+import java.util.Scanner;
 public class Multiplication
 {
     public static void main(String[] args)
     {
         System.out.println("Multiplication table");
+        System.out.print("Enter Number : ");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
         for (int i=1; i<=10; i++)
         {
-            int sum = 2*i;
-            System.out.println("2X"+i+"="+sum);
+            int sum = num*i;
+            System.out.println(num+"X"+i+"="+sum);
         }
     }
 }
