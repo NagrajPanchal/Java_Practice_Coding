00eb3564e9bd2a6d597e378cb0e533b6fdbbfd17
Nagraj <nagrajsubhashpanchal.gmail.com>
2022-09-15 14:37:15 +0530

    Refactor : Multiplication table

---
+++ src/com/bridgelabz/Multiplication.java
@@ -0,0 +1,15 @@
+// Multiplication Table
+package com.bridgelabz;
+
+public class Multiplication
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Multiplication table");
+        for (int i=1; i<=10; i++)
+        {
+            int sum = 2*i;
+            System.out.println("2X"+i+"="+sum);
+        }
+    }
+}
